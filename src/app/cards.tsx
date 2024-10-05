c60f1040e5c8878f14a63da515298687fcf038a4
Boamaa <133799103+Boamaa@users.noreply.github.com>
2024-10-05 18:11:04 +0000

    Update

--- src/app/cards.tsx
+++ src/app/cards.tsx
@@ -1,6 +1,8 @@
 import chart from './img/chart.png';
 import branch from './img/branch.png';
 import connect from './img/connect.png';
+import daisy from './img/daisy.jpg';
+import mickey from './img/mickey.jpg';
 
 export default function Cards (){
     return <div className="rect">
@@ -64,6 +66,33 @@ export default function Cards (){
     <div className="bar w-56 h-4 bg-gray-200">
         <div className="fill w-1/5 h-4 bg-black"></div>
     </div>
+</div>
+<div className="recent mt-11">
+<h2 className="head text-xl font-bold flex">
+Recent Activity
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 ml-14 mt-1 hover:text-gray-500">
+  <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+</svg>
+</h2>
+<div className="people mt-7">
+  <div className="person flex">
+    <img src={daisy.src} alt="" className='flex w-8 h-8 rounded-full' />
+    <div className="decr flex flex-col text-xs">
+    <span className='font-semibold ml-3'>add-comments</span>
+    <span className='font-light ml-3'>Daisy <span className='text-gray-400'>opened </span>#126   <span className="dot font-bold">   •   </span><span className='text-gray-400'>2 hours ago</span></span>
+    </div>
+  </div>
+
+  <div className="person flex mt-7">
+    <img src={mickey.src} alt="" className='flex w-8 h-8 rounded-full' />
+    <div className="decr flex flex-col text-xs">
+    <span className='font-semibold ml-3'>add-comments</span>
+    <span className='font-light ml-3'>Mickey <span className='text-gray-400'>opened </span>#125   <span className="dot font-bold">   •   </span><span className='text-gray-400'>4 hours ago</span></span>
+    </div>
+  </div>
+  
+</div>
+
 </div>
 </div>
     </div>
